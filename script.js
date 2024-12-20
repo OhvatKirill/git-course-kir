9cb3e6eb3050af517a9bc9db24fb8aca8e98ec8d
OhvatKirill <kirillohvat@gmail.com>
2024-12-20 11:35:43 +0700

    modified script hren

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.body.innerHTML = '<h1>Hello from JavaScript</h1>';
-document.body.innerHTML = '<p>Вот так вот</p>'
-console.log('test 2')
+document.body.innerHTML = '<p>Вот так вот</p>';
+console.log('test 2!!!');
+console.log('hren pizda');
